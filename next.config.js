3934e92a0365173ea67458cdc8f70d9dca12365b
Dipak Majumdar <rahulmajumdar400@gmail.com>
2026-08-06 00:13:02 +0530

    nextjs fixing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  compress: true,
+  poweredByHeader: false,
+};
 
 module.exports = nextConfig;
